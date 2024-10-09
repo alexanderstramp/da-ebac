f022e97477d13ebbd65d0bcf729209e32b6912c2
alexanderstramp <tiago.history@hotmail.com>
2024-10-09 17:26:34 +0000

    Adicionando gasolina.py; gasolina.png ; gasolina.csv

--- gasolina.py
+++ gasolina.py
@@ -1,14 +1,16 @@
-# código de geração do gráfico 
+# código de geração do gráfico
 
 import seaborn as sns
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 # Lendo o arquivo csv
 gasolina_df = pd.read_csv('gasolina.csv')
 
 # gerando o grafico
-grafico = sns.barplot(data=gasolina_df, x='dia', y='venda')
+grafico = sns.lineplot(data=gasolina_df, x='dia', y='venda')
+grafico.set(title='Preço da Gasolina em SP', xlabel='Dias', ylabel='Preços')
+
 
 # salvando o grafico
 plt.savefig('gasolina.png')
